441471b8f68813f6d1414078df7d3c23bf4d5fa7
gippy-marley <gipsongippy99@gmail.com>
2021-08-25 12:02:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Bag from "./components/Bag";
+import Banner from "./components/Banner";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Order from "./components/Order";
+import data from "./data.js";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Orderpage from "./components/Orderpage";
+import About from "./components/About";
 
 function App() {
+  const { products } = data;
+  const [cartItems, setCartItems] = useState([]);
+  const onAdd = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist) {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...product, qty: 1 }]);
+    }
+  };
+
+  const onRemove = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (cartItems.length === 1 && exist.qty === 1) {
+      alert("please add some items");
+      setCartItems(cartItems.filter((x) => x.id !== product.id));
+    }
+    if (exist.qty === 1) {
+      setCartItems(cartItems.filter((x) => x.id !== product.id));
+    } else {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
+        )
+      );
+    }
+  };
+
+  const itemsPrice = cartItems.reduce((a, c) => a + c.qty * c.price, 0);
+  const taxPrice = itemsPrice * 0.14;
+  const shippingPrice = 0;
+  const totalPrice = itemsPrice + taxPrice + shippingPrice;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/about">
+            <About></About>
+          </Route>
+          <Route exact path="/">
+            <Banner></Banner>
+
+            <div className="row">
+              <Main
+                onAdd={onAdd}
+                onRemove={onRemove}
+                cartItems={cartItems}
+                products={products}
+              ></Main>
+
+              <Bag
+                onAdd={onAdd}
+                onRemove={onRemove}
+                totalPrice={totalPrice}
+                shippingPrice={shippingPrice}
+                taxPrice={taxPrice}
+                itemsPrice={itemsPrice}
+                cartItems={cartItems}
+              ></Bag>
+            </div>
+
+            <Order totalPrice={totalPrice} cartItems={cartItems}></Order>
+          </Route>
+
+          <Route exact path="/ordersuccess">
+            <Orderpage></Orderpage>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
